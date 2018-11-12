7b74aac346f2b875628bbebd6f25ff1c06022326
suXin <suxinjke@gmail.com>
2018-11-12 16:31:51 +0700

    transliterate cyrillic in twitch chat messages

--- twitch/twitch.cpp
+++ twitch/twitch.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <regex>
 #include <fstream>
+#include "../utf8/utf8.h"
 #include "cpp_aux.h"
 
 void event_connect( irc_session_t *session, const char *event, const char *origin, const char **params, unsigned int count ) {
@@ -26,15 +27,98 @@ void event_channel( irc_session_t *session, const char *event, const char *origi
 		return;
 	}
 
-	std::string message = params[1];
+	std::string originalMessage = params[1];
 
-	ctx->twitch->OnMessage( origin, message );
+	ctx->twitch->OnMessage( origin, originalMessage );
 
-	message = std::regex_replace( message, std::regex( "[^ -~]" ), "" );
-	message = std::regex_replace( message, std::regex( "\\s+" ), " " );
-	message = std::regex_replace( message, std::regex( "^[#!0]*([1-9]{1})" ), "" );
+	static std::map<int, std::string> translitDictionary = {
+		{ 1040, "A" }, // А
+		{ 1072, "a" }, // а
+		{ 1041, "B" }, // Б
+		{ 1073, "b" }, // б
+		{ 1042, "V" }, // В
+		{ 1074, "v" }, // в
+		{ 1043, "G" }, // Г
+		{ 1075, "g" }, // г
+		{ 1044, "D" }, // Д
+		{ 1076, "d" }, // д
+		{ 1045, "E" }, // Е
+		{ 1077, "e" }, // е
+		{ 1025, "Yo" }, // Ё
+		{ 1105, "yo" }, // ё
+		{ 1046, "Zh" }, // Ж
+		{ 1078, "zh" }, // ж
+		{ 1047, "Z" }, // З
+		{ 1079, "z" }, // з
+		{ 1048, "Y" }, // И
+		{ 1080, "i" }, // и
+		{ 1049, "J" }, // Й
+		{ 1081, "j" }, // й
+		{ 1050, "K" }, // К
+		{ 1082, "k" }, // к
+		{ 1051, "L" }, // Л
+		{ 1083, "l" }, // л
+		{ 1052, "M" }, // М
+		{ 1084, "m" }, // м
+		{ 1053, "N" }, // Н
+		{ 1085, "n" }, // н
+		{ 1054, "O" }, // О
+		{ 1086, "o" }, // о
+		{ 1055, "P" }, // П
+		{ 1087, "p" }, // п
+		{ 1056, "R" }, // Р
+		{ 1088, "r" }, // р
+		{ 1057, "S" }, // С
+		{ 1089, "s" }, // с
+		{ 1058, "T" }, // Т
+		{ 1090, "t" }, // т
+		{ 1059, "U" }, // У
+		{ 1091, "u" }, // у
+		{ 1060, "F" }, // Ф
+		{ 1092, "f" }, // ф
+		{ 1061, "Kh" }, // Х
+		{ 1093, "kh" }, // х
+		{ 1062, "Ts" }, // Ц
+		{ 1094, "ts" }, // ц
+		{ 1063, "Ch" }, // Ч
+		{ 1095, "ch" }, // ч
+		{ 1064, "Sh" }, // Ш
+		{ 1096, "sh" }, // ш
+		{ 1065, "Shh" }, // Щ
+		{ 1097, "shh" }, // щ
+		{ 1067, "Y" }, // Ы
+		{ 1099, "y" }, // ы
+		{ 1069, "E" }, // Э
+		{ 1101, "e" }, // э
+		{ 1070, "Yu" }, // Ю
+		{ 1102, "yu" }, // ю
+		{ 1071, "Ya" }, // Я
+		{ 1103, "ya" }, // я
+		{ 1068, "" }, // Ь
+		{ 1100, "" }, // ь
+		{ 1066, "" }, // Ъ
+		{ 1098, "" }, // ъ
+	};
 
-	message = aux::str::trim( message );
+	std::string message = "";
+	message.reserve( originalMessage.size() );
+
+	auto it = originalMessage.begin();
+	while ( it != originalMessage.end() ) {
+		auto cp = utf8::peek_next( it, originalMessage.end() );
+		if ( translitDictionary.find( cp ) != translitDictionary.end() ) {
+			message += translitDictionary[cp];
+		} else {
+			message += *it;
+		}
+		utf8::advance( it, 1, originalMessage.end() );
+	}
+
+	aux::str::replace( &message, "[^ -~]", "" );
+	aux::str::replace( &message, "\\s+", " " );
+	aux::str::replace( &message, "^[#!0]*([1-9]{1})", "" );
+
+	aux::str::trim( &message );
 
 	if ( message.size() > 0 && message.size() < 64 ) {
 		auto &killfeedMessages = ctx->twitch->killfeedMessages;
