a26a2eb8803415b23d3ef75148f47e74f3b58f07
suXin <suxinjke@gmail.com>
2024-11-01 17:47:32 +0700

    [HL25] Backport flesh hit sound fix

--- dlls/client.cpp
+++ dlls/client.cpp
@@ -1409,6 +1409,12 @@ int AddToFullPack( struct entity_state_s *state, int e, edict_t *ent, edict_t *h
 		state->health		= ent->v.health;
 	}
 
+	CBaseEntity* pEntity = static_cast<CBaseEntity*>(GET_PRIVATE(ent));
+	if (pEntity && pEntity->Classify() != CLASS_NONE && pEntity->Classify() != CLASS_MACHINE)
+		state->eflags |= EFLAG_FLESH_SOUND;
+	else
+		state->eflags &= ~EFLAG_FLESH_SOUND;
+
 	return 1;
 }
 
